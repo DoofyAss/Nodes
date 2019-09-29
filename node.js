07057a721c2adf153d96c4f9a50a78bab0424551
Mr. Black <mr.black.developer@gmail.com>
2019-09-30 04:32:45 +1100

    canvas -> svg

--- node.js
+++ node.js
@@ -22,7 +22,7 @@ Object.prototype.each = function(c) {
 	let self = this;
 	
 	Object.keys(this).map(function(key, i) {
-		c.call(self[key], self[key], i);
+		c.call(self[key], self[key], i, key);
 	});
 }
 
@@ -39,12 +39,11 @@ Object.prototype.each = function(c) {
 	Main
 */
 
-var area = o('#area');
-var canvas = o('canvas');
+var area = o('#area svg');
 
 (document.body.onresize = () => {
-	canvas.width = canvas.clientWidth; // area.clientWidth;
-	canvas.height = canvas.clientHeight; // area.clientHeight;
+	area.viewBox.baseVal.width = area.clientWidth;
+	area.viewBox.baseVal.height = area.clientHeight;
 })();
 
 
@@ -63,119 +62,121 @@ var canvas = o('canvas');
 function Node(option) {
 	
 	Object.assign(this, {
-		x: 116,
-		y: 116,
+		x: 32,
+		y: 32,
 		w: 160,
 		h: 64,
-		r: 8,				// radius [16, 8, 16, 8]
-		f: '#ffffff',		// fill
-		s: false			// shadow
+		r: 6, // radius
+		f: '#ffffff', // fill
+		n: 'Start'
     }, option);
-}
-
-
-
-Node.prototype.hover = function(x, y) {
-	return	(this.x < x) && (this.x + this.w > x) &&
-			(this.y < y) && (this.y + this.h > y);
-}
-
-
-
-function View(canvas) {
 	
-	let view = this;
-	let context = this.context = canvas.getContext('2d');
+	
+}
+
+
+
+function View() {
 	
 	this.node = [];
 	
-	canvas.onmousemove = function(e) {
+	area.onmousemove = function(e) {
 		
 		view.node.each(function() {
-			this.s = this.hover(e.layerX, e.layerY) ? true : false;
+			
 		});
-		
-		view.draw();
 	}
 }
 
 
 
-View.prototype.draw = function() {
+var SVG = {
 	
-	this.context.clearRect(0, 0, canvas.width, canvas.height);
+	create: function(tag, attributes) {
+		
+		let element = document.createElementNS("http://www.w3.org/2000/svg", tag);
+		
+		attributes.each(function(e, i, a) {
+			element.setAttribute(a, e);
+		});
+		
+		return element;
+	}
+}
+
+var HTML = {
+
+	Node: {
 	
-	this.node.each((node) => {
-		draw.rect(
-			node.x,
-			node.y,
-			node.w,
-			node.h,
-			node.r,
-			node.f,
-			node.s
-		);
-	});
+		create: function(node) {
+		
+			let layerMain = SVG.create('svg', {
+				x: node.x,
+				y: node.y,
+				width: node.w + 16,
+				height: node.h + 16,
+				cursor: 'grab'
+			});
+			
+			let layerBody = SVG.create('rect', {
+				x: 8,
+				y: 8,
+				fill: node.f,
+				width: node.w,
+				height: node.h,
+				rx: node.r,
+				ry: node.r,
+				filter: 'url(#shadow)'
+			});
+			
+			layerMain.append(layerBody);
+			
+			return layerMain;
+		}
+	}
 }
 
 
 
 View.prototype.add = function(node) {
 	this.node.push(node);
-}
-
-
-
-var view = new View(canvas);
-view.add(new Node());
-view.add(new Node( { x: 356, y: 162, f: '#426c92' } ));
-view.add(new Node( { y: 212, f: '#33784c' } ));
-
-
-
-
-
-
-
-
-
-
-/**
-	Render
-*/
-
-var draw = {
 	
-	get context() { return view.context; },
+	node.element = HTML.Node.create(node);
+	area.append(node.element);
 	
-	
-	
-	rect: function(x, y, w, h, r, f, s) {
+	node.element.onmousedown = function() {
 		
-		if (typeof r != 'object') r = typeof r == 'number' ? [r, r, r, r] : [0, 0, 0, 0];
+		node.grab = true;
+		this.setAttribute('cursor', 'grabbing');
 		
-		let c = this.context;
-		
-		c.fillStyle = f;
-		c.shadowBlur = 0;
-		
-		c.beginPath();
-		c.moveTo(x + r[0], y);
-		c.lineTo(x + w - r[1], y);
-		c.quadraticCurveTo(x + w, y, x + w, y + r[1]);
-		c.lineTo(x + w, y + h - r[2]);
-		c.quadraticCurveTo(x + w, y + h, x + w - r[2], y + h);
-		c.lineTo(x + r[3], y + h);
-		c.quadraticCurveTo(x, y + h, x, y + h - r[3]);
-		c.lineTo(x, y + r[0]);
-		c.quadraticCurveTo(x, y, x + r[0], y);
-		c.closePath();
-		
-		if (s) {
-			c.shadowColor = '#0000009d';
-			c.shadowBlur = 6;
+		this.onmouseup = this.onmouseleave = function() {
+			node.grab = false;
+			this.setAttribute('cursor', 'grab');
+			this.onmousemove = null;
 		}
 		
-		c.fill();
+		this.onmousemove = function(e) {
+			
+			if (!node.grab) return;
+			
+			// setTimeout(function() { node.element.x.baseVal.value += e.movementX; }, 128);
+			// setTimeout(function() { node.element.y.baseVal.value += e.movementY; }, 128);
+			this.x.baseVal.value += e.movementX;
+			this.y.baseVal.value += e.movementY;
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+var view = new View();
+view.add(new Node());
+view.add(new Node( { n: 'Main', x: 296, y: 90 } ));
+view.add(new Node( { n: 'Value', y: 160 } ));
